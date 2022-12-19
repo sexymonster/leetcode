0109a0c59a97833cc866989237c1a5b5b9c5d166
이제형 <104478694+sexymonster@users.noreply.github.com>
2022-12-19 20:21:42 +0900

    Time: 30 ms (94.03%), Space: 13.7 MB (98.16%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,18 +1,8 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        numbers = []
-        temp = []
+        l = [1] * numRows
         for i in range(numRows):
-            temp.append(1)
-            if i > 0:
-                try:
-                    for j in range(1,len(protemp)):
-                        temp.append(protemp[j-1]+protemp[j])
-                except:
-                    pass
-                temp.append(1)
-            numbers.append(temp)
-            protemp = temp
-            temp = []
-        return numbers
-        
+            l[i] = [1]*(i+1)
+            for j in range(1,i):
+                l[i][j] = l[i-1][j-1] + l[i-1][j]
+        return l
